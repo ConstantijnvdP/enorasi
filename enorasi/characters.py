a7607579f47cb4a9577cefecdb367db15fe48000
ConstantijnvdP <constantijnvdp@gmail.com>
2023-08-01 11:00:09 -0400

    working health bar

--- enorasi/characters.py
+++ enorasi/characters.py
@@ -35,12 +35,36 @@ class CharacterPane:
         self.char = load_character(char_path)
         self.hp_current = self.char.hp_max
 
+
     def get_layout(self):
         return [
             [sg.Text(self.char.name)],
-            [sg.ProgressBar(self.char.hp_max, orientation='h', size=(20,20), key=f"{self.char.name}-HP")],
+            [
+                sg.Text(f"HP  {self.hp_current}/{self.char.hp_max}", key=f"{self.char.name}-HP"),
+                sg.ProgressBar(self.char.hp_max, orientation='h', size=(20,20), key=f"{self.char.name}-HPBar"),
+                sg.Input(key=f"{self.char.name}-DMG", enable_events=True)
+            ],
             [sg.Image(self.char.path_to_img, key=f"{self.char.name}-PROFILE")]
         ]
 
-    def update_gif(self, window):
-        window.Element(f"{self.char.name}-PROFILE").UpdateAnimation(self.char.path_to_img, time_between_frames=50)
+    
+    def set_window(self, window):
+        self._window = window
+
+
+    def update_hp(self, damage):
+        assert type(damage) is int, "Can only update HP with int!"
+        self.hp_current += damage
+        
+        if self.hp_current > self.char.hp_max:
+            self.hp_current = self.char.hp_max
+
+        if self.hp_current < 0:
+            self.hp_current = 0
+
+        self._window[f"{self.char.name}-HPBar"].UpdateBar(self.hp_current)
+        self._window[f"{self.char.name}-HP"].update(f"HP  {self.hp_current}/{self.char.hp_max}")
+
+
+    def update_gif(self):
+        self._window.Element(f"{self.char.name}-PROFILE").UpdateAnimation(self.char.path_to_img, time_between_frames=50)
--- enorasi/main.py
+++ enorasi/main.py
@@ -7,22 +7,34 @@ enorasi_gk = u"\u03B5\u03BD\u03CC\u03C1\u03B1\u03C3\u03B7" ## ενόραση
 
 
 ## UI
-#layout = [[sg.Text("DEMO")], [sg.Button("Close")]]
-gaulius = characters.CharacterPane("../data/party.json")
-layout = gaulius.get_layout()
-
-window = sg.Window(title=enorasi_gk, layout=layout)
-
 def run_gui():
+    dmg = {}
+
     while True:
         event, values = window.read(timeout=50)
-        # End program if user closes window or
-        # presses the OK button
-        if event == "Close" or event == sg.WIN_CLOSED:
+        # End program if user closes window
+        if event == sg.WIN_CLOSED:
             break
 
-        gaulius.update_gif(window)
+        elif event == f"{gaulius.char.name}-DMG":
+            try:
+                dmg[f"{gaulius.char.name}"] = int(values[f"{gaulius.char.name}-DMG"])
+            except:
+                dmg[f"{gaulius.char.name}"] = 0
+
+        elif event == "Damage":
+            gaulius.update_hp(dmg[f"{gaulius.char.name}"])
+
+        gaulius.update_gif()
 
 
 if __name__ == "__main__":
+    gaulius = characters.CharacterPane("../data/party.json")
+    layout = gaulius.get_layout()
+    layout += [[sg.Button("Damage")]]
+
+    window = sg.Window(title=enorasi_gk, layout=layout)
+
+    gaulius.set_window(window)
+
     run_gui()
